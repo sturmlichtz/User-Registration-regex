81fe5cd8cbc516eda782d9a1e2e68d9c67259ac4
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-23 11:19:11 +0530

    add validation of last name

--- user-registration.sh
+++ user-registration.sh
@@ -2,7 +2,15 @@
 echo "User Registration System needs to ensure all validations are in place during the User Entry"
 read -p "Enter first name :- " firstName
 firstNamePattern="^([A-Z]{1})+([A-Za-z0-9]{2,})"
+read -p "Enter Last name :- " lastName
+lastNamePattern="^([A-Z]{1})+([A-Za-z0-9]{2,})"
 if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "valid"
+else
+	echo "not valid"
+fi
+if [[ $lastName =~ $lastNamePattern ]]
 then
 	echo "valid"
 else
