4714eebe9937184fc421bef8b8a21bca40d4e02c
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-23 15:42:49 +0530

    add validation of phone no

--- user-registration.sh
+++ user-registration.sh
@@ -1,18 +1,10 @@
 #!/bin/bash -x
 echo "User Registration System needs to ensure all validations are in place during the User Entry"
-read -p "Enter first name :- " firstName
-firstNamePattern="^([A-Z]{1})+([A-Za-z0-9]{2,})"
-read -p "Enter Last name :- " lastName
-lastNamePattern="^([A-Z]{1})+([A-Za-z0-9]{2,})"
-if [[ $firstName =~ $firstNamePattern ]]
+read -p "Enter phone no to validate :- " phone
+phone_Pattern="^([0-9]{2})([ ]{1})([0-9]{10})$"
+if [[ $phone =~ $phone_Pattern ]]
 then
-	echo "valid"
+	echo "valid phone no."
 else
-	echo "not valid"
-fi
-if [[ $lastName =~ $lastNamePattern ]]
-then
-	echo "valid"
-else
-	echo "not valid"
+	echo "not a valid phone no."
 fi
